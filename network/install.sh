5ce44883a5fa5187449d8766e1eb6749a2a9b10f
zkarim-ib <zkarim@infoblox.com>
2019-10-29 23:51:02 -0700

    Fix installation bug

--- network/install.sh
+++ network/install.sh
@@ -7,20 +7,20 @@ apt install udhcpd
 touch /var/lib/misc/udhcpd.leases
 
 # make backups of existing files
-if [ -e /etc/rc.local ]
-then
-    cp /etc/rc.local /etc/rc.local.adhoc_bak
-fi
+# if [ -e /etc/rc.local ]
+# then
+#     cp /etc/rc.local /etc/rc.local.adhoc_bak
+# fi
 
-if [ -e /etc/udhcpd.conf ]
-then
-    cp /etc/udhcpd.conf /etc/udhcpd.conf.adhoc_bak
-fi
+# if [ -e /etc/udhcpd.conf ]
+# then
+#     cp /etc/udhcpd.conf /etc/udhcpd.conf.adhoc_bak
+# fi
 
-if [ -e /etc/dhcpcd.conf ]
-then
-    cp /etc/dhcpcd.conf /etc/dhcpcd.conf.adhoc_bak
-fi
+# if [ -e /etc/dhcpcd.conf ]
+# then
+#     cp /etc/dhcpcd.conf /etc/dhcpcd.conf.adhoc_bak
+# fi
 
 # copy new files
 cp rc.local /etc
