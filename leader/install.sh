8cc38c8beff75719dbf8babf5b736a820408def8
zkarim-ib <zkarim@infoblox.com>
2019-10-29 23:37:55 -0700

    Add network package

--- leader/install.sh
+++ leader/install.sh
@@ -5,42 +5,4 @@ if [ "$EUID" -ne 0 ]
   return 1
 fi
 
-echo "INFO: Configuring RPiLEDNetwork"
-
-NetworkInterface="
-auto lo
-iface lo inet loopback
-iface eth0 inet dhcp
- 
-auto wlan0
-iface wlan0 inet static
-  address 192.168.1.1
-  netmask 255.255.255.0
-  wireless-channel 1
-  wireless-essid RPiLEDNetwork
-  wireless-mode ad-hoc
-"
-mv /etc/network/interfaces /etc/network/interfaces-backup
-echo "$NetworkInterface" >> /etc/network/interfaces
-
-ifdown wlan1
-ifup wlan1
-
-# echo "INFO: Configuring dhcp"
-
-# sudo apt-get update
-# sudo apt-get install dhcp3-server
-
-# DHCPConfig="
-# ddns-update-style interim;
-# default-lease-time 600;
-# max-lease-time 7200;
-# authoritative;
-# log-facility local7;
-# subnet 192.168.1.0 netmask 255.255.255.0 {
-#   range 192.168.1.5 192.168.1.150;
-# }
-# "
-
-# echo "$DHCPConfig" > /etc/dhcp/dhcpd.conf
-echo "Installation complete. Restart the Pi, and RPiLEDNetwork will be an available WiFi network"
+#TODO
--- makefile
+++ makefile
@@ -1,12 +1,20 @@
 install-leader:
+	chmod +x network/install.sh
+	network/install.sh
 	chmod +x leader/install.sh
 	leader/install.sh
 install-follower:
+	chmod +x network/install.sh
+	network/install.sh
 	chmod +x follower/install.sh
 	follower/install.sh
 uninstall-leader:
+	chmod +x network/uninstall.sh
+	network/uninstall.sh
 	chmod +x leader/uninstall.sh
 	leader/uninstall.sh
 uninstall-follower:
+	chmod +x network/uninstall.sh
+	network/uninstall.sh
 	chmod +x follower/uninstall.sh
 	follower/uninstall.sh
